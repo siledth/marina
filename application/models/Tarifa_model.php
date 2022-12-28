ebf75fd1e05333a7e45bece0bb5ef516fa0b5f96
siledth <siledth@gmail.com>
2022-12-28 12:56:34 -0400

    cambios marina  27 dic 2022 se arreglo el id en tarifa xq no incrementaba

--- application/models/Tarifa_model.php
+++ application/models/Tarifa_model.php
@@ -11,7 +11,23 @@ class Tarifa_model extends CI_Model {
     }
 
     public function saves($data) {
-        return $this->db->insert('public.tarifa', $data);
+        $this->db->select('max(e.id_tarifa) as id');// esto busca el ultimo numero en la tabla 
+        $query = $this->db->get('public.tarifa e');
+        $response3 = $query->row_array();
+        $id = $response3['id'] + 1 ; // aca al ultimo numero le sumo 1 para que sea el id 
+
+
+        $datos2 = array(
+
+            'id_tarifa'=> $id,
+            'desc_concepto'=> $data['desc_concepto'],
+            'desc_condicion'=> $data['desc_condicion'],
+            'desc_tarifa'=> $data['desc_tarifa'],
+            'des_unidad'=> $data['des_unidad'],
+            'fecha'=> $data['fecha']            
+        );         
+
+        return $this->db->insert('public.tarifa', $datos2);
     }
 
     public function delete_entry($id) {
