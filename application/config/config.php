8331d94272abc560c53d4351fbfc997942315f4e
siledth <75400808+siledth@users.noreply.github.com>
2024-05-10 10:57:21 -0400

    comin2

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-//$config['base_url'] = 'http://192.168.0.150/marina';
- $config['base_url'] = 'http://localhost/marina';
+$config['base_url'] = 'http://192.168.0.150/marina';
+// $config['base_url'] = 'http://localhost/marina';
 /*
 |--------------------------------------------------------------------------
 | Index File
