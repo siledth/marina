6645a8e0b53d9b87ac1baf01cb88a7bafcfc4439
siledth <siledth@gmail.com>
2025-05-14 08:36:53 -0400

    marina

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,7 +15,7 @@ class Home extends CI_Controller
         $resultado = $this->Maletero_model->generar_deudas_maleteros();
 
         $this->load->view('templates/header.php');
-        $this->load->view('templates/navigator.php', $data);
+        $this->load->view('templates/navigator.php',);
         $this->load->view('home/index.php');
         $this->load->view('templates/footer.php');
     }
